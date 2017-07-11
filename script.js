caefcebbcc95886359d8e58cb80229447df21aa4
Jesse Cook <demouser@MacBook-Pro.local>
2017-07-11 14:25:20 -0700

    ~

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+function timesTwo(input){
+  return input * 2;
+}
+
+function timesSix(number){
+  var doubleNumber = timesTwo(number);
+  return doubleNumber * 3;
+}
