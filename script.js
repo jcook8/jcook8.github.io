3d7b4b1e70e9e6c58785bda27c8528968877654f
Jesse Cook <demouser@MacBook-Pro.local>
2017-07-17 09:14:22 -0400

    I don't care

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+$('div.site-header').html("<nav class='site-navigation'> \
+  <a href='home.html' class='navlink' id='home-nav'>Home</a>\
+  <a href='aboutme.html' class='navlink' id='about-me-nav'>About Me</a>\
+  <h2 class='navlink' id='web-title'><a href='home.html' >Jesse's Blog</a></h2>\
+  <a href='games.html' class='navlink' id='games-nav'>Games</a>\
+  <a href='gallery.html' class='navlink' id='gallery-nav'>Gallery</a>\
+</nav>");
+
+
+
+
 $('a.navlink').mouseenter( function (){$(this).fadeTo(100, 1)}).mouseleave( function (){$(this).fadeTo(200, 0.5)});
 $('#submit-button').click(submitButtonFadeAnimation);
 
@@ -7,22 +18,24 @@ function submitButtonFadeAnimation(){
 }
 
 function placeCommentBoxOnPage(){
-  $("<section class='new-comment'><h3 class='new-comment-username'></h3><p class='new-comment-content'></p></section>").appendTo('#insert-comments');
+  $("<section class='new-comment'><h3 class='new-comment-username'></h3><p class='new-comment-content'></p></section>").appendTo('.insert-comments');
 }
 
-var commentNumberArray = [0];
+var newCommentArray = [0];
+var newUserNameArray = [0];
+var newContentArray = [0];
 function changeClass(){
   if ( $('.new-comment')[0]){
-    $('.new-comment').removeClass().addClass('new-comment' + "-" + (commentNumberArray.length - 1));
-    commentNumberArray.push(0);
+    $('.new-comment').removeClass().addClass('new-comment' + "-" + (newCommentArray.length - 1));
+    newCommentArray.push(0);
   }
   if ( $('.new-comment-username')[0]){
-    $('.new-comment-username').removeClass().addClass('new-comment-username' + "-" + (commentNumberArray.length - 1));
-    commentNumberArray.push(0);
+    $('.new-comment-username').removeClass().addClass('new-comment-username' + "-" + (newUserNameArray.length - 1));
+    newUserNameArray.push(0);
   }
   if ( $('.new-comment-content')[0]){
-    $('.new-comment-content').removeClass().addClass('new-comment-content' + "-" + (commentNumberArray.length - 1));
-    commentNumberArray.push(0);
+    $('.new-comment-content').removeClass().addClass('new-comment-content' + "-" + (newContentArray.length - 1));
+    newContentArray.push(0);
   }
 }
 
@@ -30,8 +43,10 @@ function addComment(){
   var newUserName = $('#user-name-input').val();
   var newUserComment = $('#user-comment-input').val();
   placeCommentBoxOnPage();
-  $('.new-comment-username').append(newUserName);
-  $('.new-comment-content').append(newUserComment);
+  $('.new-comment-username').text(newUserName);
+  $('#user-name-input').val('');
+  $('.new-comment-content').text(newUserComment);
+  $('#user-comment-input').val('');
   changeClass();
 }
 
@@ -45,7 +60,7 @@ function handleKey(event){
 
 function submitComment (){
   $('#submit-button').click(addComment);
-  $('#compose-comment').on('keypress', handleKey);
+  $('.compose-comment').on('keypress', handleKey);
 }
 
 $(document).ready(submitComment);
